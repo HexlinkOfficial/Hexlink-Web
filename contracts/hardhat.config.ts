edec9119b6db193b3de236f02a1f06dca29c6818
dongshu2013 <dongs2011@gmail.com>
2022-08-25 20:40:41 -0700

    re-init project

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,10 +1,13 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-import "hardhat-deploy";
+
+dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,23 +25,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-     goerli: {
-      url: process.env.HARDHAT_GOERLI_URL || "",
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
       accounts:
-        process.env.HARDHAT_ACCOUNT_PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
-    enabled: process.env.HARDHAT_REPORT_GAS !== undefined,
+    enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
-  path: {
-      deploy: 'deploy',
-      deployments: 'deployments'
-  }
 };
 
 export default config;

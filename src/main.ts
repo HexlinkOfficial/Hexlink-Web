f4bd5cbaf4f80c0fbc7a91b159d59322c2c63818
dongshu2013 <dongs2011@gmail.com>
2022-08-26 22:03:48 -0700

    add vuetify support

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import { initializeApp } from "firebase/app";
 
-import './assets/main.css'
+loadFonts()
 
-const app = createApp(App)
+const firebaseConfig = {
+  apiKey: import.meta.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+};
+initializeApp(firebaseConfig);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')

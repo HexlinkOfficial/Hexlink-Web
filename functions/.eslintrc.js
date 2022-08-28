ae52bce4be03dd81eb4fc4e9c75afd164431af57
dongshu2013 <dongs2011@gmail.com>
2022-08-28 10:15:50 -0700

    merge functions and contracts

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -3,18 +3,23 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    browser: false,
+    mocha: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
-    "google",
     "plugin:@typescript-eslint/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
+    ecmaVersion: 12,
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
@@ -26,5 +31,9 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
+    ],
   },
 };

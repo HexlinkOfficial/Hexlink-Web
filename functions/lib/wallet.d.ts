337edb0bac4280f1c7249f7047d66184599d9f69
dongshu2013 <dongs2011@gmail.com>
2022-11-02 19:57:25 -0700

    remove lib from git repo

--- functions/lib/wallet.d.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from "firebase-functions";
-export declare const metadata: functions.HttpsFunction & functions.Runnable<any>;
-export declare const deployWallet: functions.HttpsFunction & functions.Runnable<any>;
-export declare const sendETH: functions.HttpsFunction & functions.Runnable<any>;
-export declare const sendERC20: functions.HttpsFunction & functions.Runnable<any>;
-export declare const executeTx: functions.HttpsFunction & functions.Runnable<any>;
-export declare const estimateERC20Transfer: functions.HttpsFunction & functions.Runnable<any>;
-export declare const estimateETHTransfer: functions.HttpsFunction & functions.Runnable<any>;

129755a52cfe86f8666d509071baecb53355abb9
dongshu2013 <dongs2011@gmail.com>
2022-11-02 20:54:59 -0700

    remove address translation from signin page

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -4,12 +4,12 @@
             <a-row justify="center">
                 <img
                     src="/src/assets/logo.png"
-                    height="150"
-                    width="150"
+                    height="100"
+                    width="100"
                 />
             </a-row>
             <a-row justify="center" class="title">
-                <span>Hexlink: start your web3 path</span>
+                <span>Hexlink: start your web3 journey</span>
             </a-row>
             <a-row justify="center" style="margin-top: 30px;">
                 <a-button size="large" @click="google_login">
@@ -29,54 +29,17 @@
                     <span>Sign In with Twitter</span>
                 </a-button>
             </a-row>
-            <a-row justify="center" style="margin-top: 30px; width: 100%;">
-                <a-input-search
-                    v-model:value="email"
-                    placeholder="Input your email"
-                    enter-button="Check Address"
-                    @search="onTranslate()"
-                    style="max-width: 500px;"
-                >
-                </a-input-search>
-            </a-row>
-            <a-row justify="center" style="margin-top: 10px; width: 100%;">
-                <a-spin v-if="translating"></a-spin>
-                <a-typography-paragraph
-                    v-if="address"
-                    :copyable="{ text: address }"
-                    style="margin-top: 10px;"
-                >
-                    Your address is {{ address }}
-                </a-typography-paragraph>
-            </a-row>
         </a-col>
     </a-row>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import { useRouter } from 'vue-router'; 
 import { googleSocialLogin, twitterSocialLogin } from '@/services/auth'
 import { useAuthStore } from '@/stores/auth'
-import { validateEmail } from '@/services/validator';
-import { accountAddress } from "@/services/web3/account";
-import { message } from "ant-design-vue";
 
 const store = useAuthStore();
 const router = useRouter();
-const email = ref<string>("");
-const address = ref<string>("");
-const translating = ref<boolean>(false);
-
-const onTranslate = async () => {
-    translating.value = true
-    if (validateEmail(email.value)) {
-        address.value = await accountAddress(email.value);
-    } else {
-        message.warning("Please input a valid email")
-    }
-    translating.value = false;
-}
 
 const google_login = async () => {
     await googleSocialLogin();
@@ -91,7 +54,7 @@ const twitter_login = async () => {
 
 <style lang="less" scoped>
 .title {
-    margin: 0px 0px 20px 0px;
+    margin: 10px 0px 40px 0px;
     font-family: system-ui;
     font-size: 1.5em;
     font-weight: bold;

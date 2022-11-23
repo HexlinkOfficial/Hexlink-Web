2b6adc46d6e388bc8fc26e2310cb806b86acbef4
pette1999 <peterchen.eth@gmail.com>
2022-11-23 04:52:59 -0500

    update on the navbar header and side bar and the overall layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div className="app">
+    <div id="preloader" v-show="isLoading"><i>.</i><i>.</i><i>.</i></div>
     <!-- <sidebar /> -->
     <router-view />
   </div>
@@ -9,8 +10,22 @@
   <router-view />
 </template> -->
 
-<script setup>
-  import Sidebar from "@/components/Sidebar.vue"
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      isLoading: true,
+      time: 100,
+      show: 5,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  },
+};
 </script>
 
 <style lang="less">
@@ -18,15 +33,92 @@
   max-height: 100vh;
 }
 
-:root {
-  --primary: #4ade80;
-  --primary-alt: #22c55e;
-  --grey: #64748b;
-  --dark: #1e293b;
-  --dark-alt: #334155;
-  --light: #f1f5f9;
-  --sidebar-width: 300px;
-}
+#preloader {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background-color: #fff;
+  z-index: 999999999; }
+  #preloader i {
+    display: block;
+    width: 16px;
+    height: 16px;
+    background: black;
+    border-radius: 16px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -8px 0 0 -8px;
+    opacity: 1;
+    -webkit-transform: translate3d(60px, 0, 0);
+    overflow: hidden;
+    text-indent: -9999px;
+    border: 1px solid white; }
+    #preloader i:nth-child(1) {
+      background: #556ee6;
+      -webkit-animation: googleDotA 1.75s ease-in-out infinite; }
+    #preloader i:nth-child(2) {
+      background: #34c38f;
+      -webkit-animation: googleDotB 1.75s ease-in-out infinite; }
+    #preloader i:nth-child(3) {
+      background: #f1b44c;
+      -webkit-animation: googleDotC 1.75s ease-in-out infinite; }
+
+@-webkit-keyframes googleDotA {
+  0% {
+    opacity: 0;
+    transform: translate3d(60px, 0, 0);
+    -webkit-transform: translate3d(60px, 0, 0); }
+  30% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0); }
+  70% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0); }
+  100% {
+    opacity: 0;
+    transform: translate3d(-300px, 0, 0);
+    -webkit-transform: translate3d(-300px, 0, 0); } }
+
+@-webkit-keyframes googleDotB {
+  0% {
+    opacity: 0;
+    transform: translate3d(180px, 0, 0);
+    -webkit-transform: translate3d(180px, 0, 0); }
+  35% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0); }
+  77% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0); }
+  100% {
+    opacity: 0;
+    transform: translate3d(-180px, 0, 0);
+    -webkit-transform: translate3d(-180px, 0, 0); } }
+
+@-webkit-keyframes googleDotC {
+  0% {
+    opacity: 0;
+    transform: translate3d(300px, 0, 0);
+    -webkit-transform: translate3d(300px, 0, 0); }
+  40% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0); }
+  80% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0); }
+  100% {
+    opacity: 0;
+    transform: translate3d(-60px, 0, 0);
+    -webkit-transform: translate3d(-60px, 0, 0); } }
 
 * {
   margin: 0;
@@ -35,7 +127,7 @@
 }
 
 body {
-  background: var(--light);
+  background: #f6f6f6;
 }
 
 button {

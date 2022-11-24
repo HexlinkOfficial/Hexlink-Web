346f0989d1eddf1d1fb0d79475f75265d6ddfb06
pette1999 <peterchen.eth@gmail.com>
2022-11-24 11:21:32 -0500

    update the looking of notification button and profile in the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,7 +29,9 @@
               <div class="notification dropdown" @click="activeDropDown('notification')"
                 :class="active_ === 'notification' && 'show'">
                 <div class="notify-bell" data-toggle="dropdown">
-                  <span><i class="icofont-alarm"></i></span>
+                  <svg width="15" height="20" viewBox="0 0 15 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-auto w-3 sm:w-auto"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.61379 0.309937C9.89044 0.427717 10.07 0.699327 10.07 1V7.99819H14C14.2667 7.99819 14.5133 8.13982 14.6478 8.37018C14.7822 8.60053 14.7842 8.88494 14.653 9.11714L14.1609 9.988C12.2948 13.2906 9.9149 16.2752 7.11058 18.8298L6.31506 19.5544C6.0955 19.7545 5.7786 19.8061 5.50693 19.686C5.23526 19.566 5.06 19.297 5.06 19V12.0611H1C0.585786 12.0611 0.25 11.7254 0.25 11.3111C0.25 11.177 0.285225 11.051 0.346932 10.9421C2.10572 7.69064 4.3575 4.73117 7.02222 2.16895L8.80017 0.459375C9.0169 0.250976 9.33715 0.192157 9.61379 0.309937ZM2.28458 10.5611H5.81C6.22421 10.5611 6.56 10.8969 6.56 11.3111V17.2945C8.9786 15.009 11.0517 12.3827 12.7134 9.49819H9.32C8.90579 9.49819 8.57 9.1624 8.57 8.74819V2.76162L8.06188 3.25019C5.8111 5.4144 3.86782 7.87442 2.28458 10.5611Z" fill="currentColor"></path></svg>
+                  <!-- <span><i class="icofont-alarm"></i></span> -->
+                  <span class="notify-dot"></span>
                 </div>
                 <div class="
                     dropdown-menu dropdown-menu-right
@@ -82,7 +84,9 @@
 
               <div class="profile_log dropdown" @click="activeDropDown('profile')" :class="active_ && 'show'">
                 <div class="user" data-toggle="dropdown">
-                  <span class="thumb"><img :src="user?.photoURL" :size="64" referrerpolicy="no-referrer" /></span>
+                  <div style="position: relative;">
+                    <span class="thumb"><img :src="user?.photoURL" :size="64" referrerpolicy="no-referrer" /></span>
+                  </div>
                   <span class="arrow"><i class="icofont-angle-down"></i></span>
                 </div>
                 <div class="dropdown-menu dropdown-menu-right mt-3" :class="active_ === 'profile' && 'show'">
@@ -175,11 +179,12 @@ export default {
   position: fixed;
   top: 0;
   left: 9.5rem;
-  right: 0;
+  right: 3rem;
   z-index: 2; }
   @media only screen and (max-width: 880px) {
     .header {
       left: 0;
+      right: 0;
       padding: 30px 0px 20px 0px; } }
   .header .brand-logo {
     margin-top: 0px;
@@ -238,14 +243,31 @@ cursor: pointer; }
   display: flex;
   align-items: center; }
   .profile_log .user .thumb {
-    height: 35px;
-    width: 35px;
+    display: block;
+    overflow: hidden;
+    // background-image: linear-gradient(to right, var(--tw-gradient-stops));
+    // background-color: #6366F1;
+    // background-color: #8B5CF6;
+    // background-color: #EC4899;
+    // transition-property: all;
+    width: 40px;
+    height: 40px;
+    border-radius: 50px;
+    border-color: #ffffff;
+    border-style: solid;
+    // height: 40px;
+    // width: 40px;
     // border-radius: 50px;
     color: #fff;
-    text-align: center; }
+    text-align: center;
+    // @media (min-width: 640px) {
+    //     width: 3rem;
+    //     height: 3rem; }
+    &:hover {
+      transform: translateY(-0.125rem); } }
     .profile_log .user .thumb img {
       border-radius: 50px;
-      max-width: 35px; }
+      max-width: 38px; }
   .profile_log .user .arrow i {
     font-weight: bold;
     font-size: 14px;
@@ -259,7 +281,7 @@ cursor: pointer; }
   border: 0px;
   padding: 0px;
   margin: 0px;
-  top: 25px !important;
+  // top: 25px !important;
   box-shadow: 0 1.5rem 4rem rgba(22, 28, 45, 0.15);
   border-radius: 5px;
   background-color: #fff;
@@ -362,19 +384,51 @@ cursor: pointer; }
 .notification {
   cursor: pointer; }
   .notification .notify-bell {
+    display: flex;
+    position: relative;
+    background-color: #ffffff;
+    transition-property: all;
+    justify-content: center;
+    align-items: center;
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 9999px;
+    border-width: 1px;
+    border-color: #F3F4F6;
+    cursor: pointer;
     margin-right: 30px; }
+    .notification .notify-bell:hover {
+      transform: translateY(-0.125rem);
+    }
+    @media (min-width: 640px) {
+      width: 3rem;
+      height: 3rem;
+    }
     @media only screen and (max-width: 880px) {
       .notification .notify-bell {
         margin-right: 15px;
         margin-left: 15px; } }
     .notification .notify-bell i {
-      font-size: 20px;
+      font-size: 22px;
       color: #343a40; }
+  .notification .notify-dot {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 0.625rem;
+    height: 0.625rem;
+    border-radius: 9999px;
+    opacity: 1;
+    background-color: rgb(7, 106, 224);
+    // @media (min-width: 640px) {
+    //   width: 0.75rem;
+    //   height: 0.75rem; }
+  }
   .notification .dropdown-menu {
     border: 0px;
     padding: 15px 20px 10px;
     margin: 0px;
-    top: 25px !important;
+    // top: 25px !important;
     width: 330px;
     box-shadow: 0 36px 48px rgba(27, 25, 148, 0.08);
     border-radius: 5px; }

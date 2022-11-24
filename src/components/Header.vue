0544ead650dc3027953b1aae8d7041f788a67cef
pette1999 <peterchen.eth@gmail.com>
2022-11-24 14:05:35 -0500

    implement wallet address copy with toast

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -104,9 +104,8 @@
                     <div class="user-balance">
                       <div class="address">
                         <span style="color: #4B5563; font-size: 0.875rem; line-height: 1.25rem; font-weight: 500;">Address: </span>
-                        <span :copyable="{ text: user.walletAddress }" style="padding-top: 0.25rem; padding-bottom: 0.25rem; padding-left: 0.5rem; padding-right: 0.5rem; background-color: #F3F4F6; font-size: 0.875rem; line-height: 1.25rem; border-radius: 0.5rem; ">
+                        <span @click="doCopy" style="margin-bottom: 0; padding-top: 0.25rem; padding-bottom: 0.25rem; padding-left: 0.5rem; padding-right: 0.5rem; background-color: #F3F4F6; font-size: 0.875rem; line-height: 1.25rem; border-radius: 0.5rem; ">
                           {{ addressText }}
-                          <div role="button" tabindex="0" class="ant-typography-copy" aria-label="Copy" style="border: 0px; background: transparent; padding: 0px; line-height: inherit; display: inline-block;"><span role="img" aria-label="copy" class="anticon anticon-copy"><svg focusable="false" class="" data-icon="copy" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M832 64H296c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496v688c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V96c0-17.7-14.3-32-32-32zM704 192H192c-17.7 0-32 14.3-32 32v530.7c0 8.5 3.4 16.6 9.4 22.6l173.3 173.3c2.2 2.2 4.7 4 7.4 5.5v1.9h4.2c3.5 1.3 7.2 2 11 2H704c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32zM350 856.2L263.9 770H350v86.2zM664 888H414V746c0-22.1-17.9-40-40-40H232V264h432v624z"></path></svg></span></div>
                         </span>
                       </div>
                       <!-- <div class="total">
@@ -148,12 +147,14 @@
 import { computed } from "vue";
 import { useAuthStore } from '@/stores/auth';
 import { prettyPrintAddress } from '@/services/web3/account';
+import { createToaster } from "@meforma/vue-toaster";
 
 export default {
   name: "Header",
   data() {
     const store = useAuthStore();
     const user = store.currentUser!;
+    const actualAddress = user.walletAddress;
     const addressText = computed(() => {
       if (user.walletAddress) {
         return prettyPrintAddress(user.walletAddress!);
@@ -162,6 +163,7 @@ export default {
       }
     })
     return {
+      actualAddress,
       addressText,
       user,
       active_: "",
@@ -178,6 +180,20 @@ export default {
       this.themes = this.themes !== "" ? "" : "dark-theme";
       element.classList = value_ ? value.theme : this.themes;
       localStorage.setItem("theme", value_ ? value.theme : this.themes);
+    },
+    doCopy: function () {
+      this.$copyText(this.actualAddress).then(
+        function () {
+          // alert("Copied");
+          const toaster = createToaster({ position: "top", duration: 2000 });
+          toaster.success(`Copied`);
+        },
+        function () {
+          // alert("Can not copy");
+          const toaster = createToaster({ position: "top", duration: 2000 });
+          toaster.error(`Can not copy`);
+        }
+      );
     },
   },
   created() {

b82a98354dbd40f84957288b26929f733629d12b
mingyuan1219 <mirandayuan09@gmail.com>
2023-02-18 17:32:27 -0800

    Fix tx service graphql issue

--- transaction/src/graphql/client.ts
+++ transaction/src/graphql/client.ts
@@ -1,7 +1,9 @@
 import {createClient} from "@urql/core";
+import fetch from "cross-fetch";
 
 export const client = createClient({
   url: process.env.VITE_HASURA_URL!,
+  fetch,
   fetchOptions: () => {
     return {
       headers: {"x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET!},

e8625304e75bc348d615858e1f60c4d2206be653
Wenjing Yu <zihao.chen31@gmail.com>
2023-02-26 17:16:34 -0800

    [OTP] Improved email template with beta version of logo

--- functions/src/signin.ts
+++ functions/src/signin.ts
@@ -47,17 +47,85 @@ export const genOTP = functions.https.onCall(async (data, context) => {
     from: "info@hexlink.io",
     subject: "Hexlink Login Code",
     text: `Hi,\n\nThank you for choosing Hexlink. Use the following OTP to complete your Log in procedures. OTP is valid for 5 minute.\n${plainOTP}\n\nRegards,\nHexlink`,
-    html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-      <div style="margin:10px auto;width:100%;padding:10px 0">
-        <div style="border-bottom:1px solid #eee">
-          <p href="" style="font-size:1.6em;background: #1890ff;color: #fff;text-decoration:none;font-weight:600">Hexlink</p>
-        </div>
-        <p style="font-size:1.5em">Hi,</p>
-        <p style="font-size:1.4em">Thank you for choosing Hexlink. Use the following OTP to complete your Log in procedures. OTP is valid for 5 minutes.</p>
-        <h2 style="font-size:2.5em; background: #1890ff;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${plainOTP}</h2>
-        <p style="font-size:1.4em;">Regards,<br />Hexlink</p>
-      </div>
-    </div>`,
+    html: `<html lang="en-US">
+
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <title>New Account Email Template</title>
+        <meta name="description" content="New Account Email Template.">
+        <style type="text/css">
+            a:hover {text-decoration: underline !important;}
+        </style>
+    </head>
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+        <!-- 100% body table -->
+        <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+            style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+                <td>
+                    <table style="background-color: #f2f3f8; max-width:670px; margin:0 auto;" width="100%" border="0"
+                        align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                                <a href="play.hexlink.io" title="logo" target="_blank">
+                                <img width="300" src="https://i.postimg.cc/HkJWJSNj/hexlink.png" title="logo" alt="logo">
+                              </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                    style="max-width:670px; background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 35px;">
+                                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Get started
+                                            </h1>
+                                            <p style="font-size:15px; color:#455056; margin:8px 0 0; line-height:24px;">
+                                                Thank you for choosing Hexlink. Use the following OTP to complete your Log in procedures. 
+                                                <br><strong>OTP is valid for 5 minutes</strong>.</p>
+                                            <span
+                                                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                            <p
+                                                style="color:#455056; font-size:18px;line-height:20px; margin:0; font-weight: 500;">
+                                                <strong
+                                                    style="display: block;font-size: 13px; margin: 0 0 4px; color:rgba(0,0,0,.64); font-weight:normal;">Verification Code</strong>
+                                                    <h2 style="font-size:2.5em; background: #1890ff;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${plainOTP}</h2>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                                <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>play.hexlink.io</strong> </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <!--/100% body table-->
+    </body>
+    </html>`,
   });
   return {code: 200, sentAt: new Date().getTime()};
 });

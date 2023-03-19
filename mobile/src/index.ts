78c0214690dd4905c40174d75ec892686d3cad9d
Yang Xi <yang@Yangs-MacBook-Pro.local>
2023-03-19 00:28:15 -0700

    remove the uncomment code

--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -8,11 +8,4 @@ app.use(express.json());
 
 app.use('/auth-app', authAppController.authAppSetupController);
 
-// app.use('/auth-app', authRouter);
-
-// app.get('/auth-app', (req: Request, res: Response) => {
-//     console.log(req.params);
-//     res.status(200).json({ message: "success" });
-// });
-
 app.listen(8017);

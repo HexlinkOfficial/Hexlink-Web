3e2a58e3d03485a7ddfab1dd0114faf495137c7d
Yang Xi <yang@Yangs-MacBook-Pro.local>
2023-03-19 00:25:39 -0700

    update the client and server req-res on Auth App setup

--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import authRouter from './router/auth-router';
+import * as authAppController from './controller/auth-app-controller';
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.use('/auth-app', authRouter);
+app.use('/auth-app', authAppController.authAppSetupController);
+
+// app.use('/auth-app', authRouter);
+
+// app.get('/auth-app', (req: Request, res: Response) => {
+//     console.log(req.params);
+//     res.status(200).json({ message: "success" });
+// });
 
 app.listen(8017);

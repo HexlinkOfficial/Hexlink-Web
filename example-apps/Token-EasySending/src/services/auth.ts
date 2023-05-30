69ce1a37ed707a194cf73bce5b9582c8f244015f
dongshu2013 <dongs2011@gmail.com>
2023-05-30 03:14:23 -0700

    remove login

--- example-apps/Token-EasySending/src/services/auth.ts
+++ example-apps/Token-EasySending/src/services/auth.ts
@@ -2,13 +2,12 @@ import {
     getAuth,
     GoogleAuthProvider,
     TwitterAuthProvider,
-    signInWithCustomToken,
+    signInAnonymously,
     signInWithPopup,
     signOut,
 } from '@firebase/auth';
-import type { User } from '@firebase/auth';
 import type { IUser } from "@/types";
-import { getFunctions, httpsCallable,  type HttpsCallable } from '@firebase/functions';
+import { getFunctions, httpsCallable } from '@firebase/functions';
 import { app } from '@/services/firebase';
 import { useAuthStore } from "@/stores/auth";
 import { switchNetwork } from "@/web3/network";
@@ -16,17 +15,12 @@ import { GOERLI, SUPPORTED_CHAINS, type Chain } from "../../../../functions/comm
 import { useChainStore } from '@/stores/chain';
 import { initTokenList } from "@/web3/tokens";
 import { useTokenStore } from '@/stores/token';
-import * as jose from 'jose'
-import { ethers } from "ethers";
+import {validateEmail, createNotification} from "@/web3/utils";
 
-import type {Provider} from "@ethersproject/providers";
-import { Hexlink__factory } from '@hexlink/contracts';
-import { hexlContract } from "../../../../functions/common/src/hexlink"
 import type { Token } from "../../../../functions/common";
 
 const auth = getAuth(app)
 const functions = getFunctions()
-const hexlinkInterface = Hexlink__factory.createInterface();
 
 export async function genOTP(email: string, mode: string, sender?: string, sendAmount?: string, token?: Token) {
     const genOTPCall = httpsCallable(functions, 'genOTP');
@@ -34,76 +28,46 @@ export async function genOTP(email: string, mode: string, sender?: string, sendA
     return (result.data as any).code as number;
 }
 
-export async function validateOTP(email: string, otp: string) {
-    const validateOTPCall = httpsCallable(functions, 'validateOTP');
-    const result = await validateOTPCall({email, otp, action: "genToken"});
-    const resultData = result.data as any;
-    if (resultData.code !== 200) {
-        return {code: resultData.code, message: resultData.message}
-    }
+export async function refreshToken() {
+    const idToken = await auth.currentUser!.getIdToken(true);
+    const store = useAuthStore();
+    store.refreshIdToken(idToken);
+}
 
+export async function emailAnonymousLogin(email: string) {
     try {
-        const userCredential = await signInWithCustomToken(auth, resultData.token);
-        const cred = userCredential.user;
-        const idToken = await getIdTokenAndSetClaimsIfNecessary(cred);
         email = email.toLowerCase();
+        if (!validateEmail(email)) {
+            createNotification("invalid email address", "error");
+            return;
+        }
+        const result = await signInAnonymously(auth);
+        const idToken = await result.user!.getIdToken();
         const user : IUser = {
-            provider: "email",
+            provider: "hexlink.io",
             idType: "mailto",
-            email,
+            email: email,
             handle: email,
             name: `mailto:${email}`,
-            uid: cred.uid,
-            providerUid: email,
-            displayName: "Anonymous",
-            idToken,
+            uid: result.user.uid,
+            providerUid: result.user.uid, // TODO: ensure this is google uid
+            displayName: result.user.displayName || undefined,
+            photoURL: result.user.photoURL || undefined,
+            idToken
         };
         useAuthStore().signIn(user);
         await init();
-        return {code: 200};
-    } catch (error) {
+    } catch(error: any) {
         console.log(error);
+        createNotification(error.message, "error");
     }
-}
-
-export async function getIdTokenAndSetClaimsIfNecessary(user: User, refresh: boolean = false) {
-    let idToken = await user.getIdToken(refresh)
-    const idTokenResult = await user.getIdTokenResult()
-    if (!idTokenResult.claims['https://hasura.io/jwt/claims']) {
-        const refreshToken = httpsCallable(functions, 'refreshToken')
-        try {
-            await refreshToken()
-            idToken = await user.getIdToken(true)
-        } catch (error: any) {
-            console.error(
-                "Unable to refresh token, which doesn't have Hasura claim.",
-            )
-            signOutFirebase()
-            throw(error)
-        }
-    }
-    if (import.meta.env.VITE_USE_FUNCTIONS_EMULATOR === 'true') {
-        const secret = new TextEncoder().encode(
-            "DkMEqQV1ZtLnTCGQOdtce5TfhpHY74ob"
-        );
-        return await new jose.SignJWT(jose.decodeJwt(idToken))
-            .setProtectedHeader({ alg: "HS256" })
-            .sign(secret);
-    }
-    return idToken
-}
-
-export async function refreshToken() {
-    const idToken = await getIdTokenAndSetClaimsIfNecessary(auth.currentUser!, true);
-    const store = useAuthStore();
-    store.refreshIdToken(idToken);
 }
 
 export async function googleSocialLogin() {
     const provider = new GoogleAuthProvider();
     try {
         const result = await signInWithPopup(auth, provider)
-        const idToken = await getIdTokenAndSetClaimsIfNecessary(result.user);
+        const idToken = await result.user!.getIdToken();
         const email = result.user.email!.toLowerCase();
         const user : IUser = {
             provider: "google.com",
@@ -122,6 +86,8 @@ export async function googleSocialLogin() {
     } catch (error: any) {
         if (error.code == 'auth/popup-closed-by-user') {
             return
+        } else {
+            createNotification(error.message, "error");
         }
     }
 }
@@ -130,7 +96,7 @@ export async function twitterSocialLogin() {
     const provider = new TwitterAuthProvider();
     try {
         const result = await signInWithPopup(auth, provider);
-        const idToken = await getIdTokenAndSetClaimsIfNecessary(result.user);
+        const idToken = await result.user!.getIdToken();
         const handle = result.user.reloadUserInfo.screenName;
         const user : IUser = {
             provider: "twitter.com",
@@ -145,8 +111,9 @@ export async function twitterSocialLogin() {
         };
         useAuthStore().signIn(user);
         await init();
-    } catch (error) {
+    } catch (error: any) {
         console.log(error);
+        createNotification(error.message, "error");
     }
 }
 
@@ -163,7 +130,7 @@ export async function genSignature(otp: string, message: string) {
         throw new Error("supported identity type");
     }
     const validateOTPCall = httpsCallable(functions, 'validateOTP');
-    const inputParam : any = {email: user.email!, otp, action: "sign", message};
+    const inputParam : any = {email: user.email!, otp, message};
     const result = await validateOTPCall(inputParam);
     return result.data;
 }
--- example-apps/Token-EasySending/src/views/SignInView.vue
+++ example-apps/Token-EasySending/src/views/SignInView.vue
@@ -2,51 +2,26 @@
   <div class="login-card" :style="{ backgroundImage: `url(${background})` }">
     <div class="card" :style="!show ? 'display: flex; align-items: center; justify-content: center;' : ''">
       <form @submit="onSubmit">
-        <transition name="fade">
-            <div v-if="show" class="step1">
-                <div style="display: flex; align-items: center; justify-content: center; margin-bottom: 1.5rem;">
-                    <img src="@/assets/svg/logo-beta.svg" style="height: 2.5rem;" />
-                </div>
-                <h2 class="title">Welcome To Hexlink</h2>
-                <p class="subtitle">Crypto for everyone</p>
-                <div class="social-login">
-                    <Button size="large" @click="google_login" type="primary" class="google-btn" :loading="isGoogleLoading">
-                        <i class="fa fa-google"></i>
-                        <span style="margin: 0 5px;">Sign in with Google</span>
-                    </Button>
-                </div>
-                <p class="or"><span>or</span></p>
-                <div class="email-login">
-                    <input type="text" v-model="email" placeholder="Enter Email" name="uname" class="email-input" required>
-                </div>
-                <Button class="cta-btn" type="primary" :loading="isLoadingLogin" :disabled="loginDisabled" @click="sendOTP">
-                    Log In
+        <div v-if="show" class="step1">
+            <div style="display: flex; align-items: center; justify-content: center; margin-bottom: 1.5rem;">
+                <img src="@/assets/svg/logo-beta.svg" style="height: 2.5rem;" />
+            </div>
+            <h2 class="title">Welcome To Hexlink</h2>
+            <p class="subtitle">Crypto for everyone</p>
+            <div class="social-login">
+                <Button size="large" @click="googleLogin" type="primary" class="google-btn" :loading="isGoogleLoading">
+                    <i class="fa fa-google"></i>
+                    <span style="margin: 0 5px;">Sign in with Google</span>
                 </Button>
             </div>
-        </transition>
-        <transition name="fade">
-            <div v-if="!show" class="step2">
-                <div style="display: flex; align-items: center; justify-content: center; margin-bottom: 1.5rem;">
-                    <img :src="secure" style="width:2.5rem; height: 2.5rem;" />
-                </div>
-                <h2 class="title">Enter Verification Code</h2>
-                <p class="subtitle">Enter code that we have sent to your email <b>{{ email }}</b></p>
-                <div class="social-login" style="flex-direction: column;">
-                    <div style="display: flex; align-items: center; justify-content: space-between;">
-                        <input v-for="(arr, index) in code" :key="index" type="number" pattern="\d*" :id="'input_' + index"
-                            maxlength="1" v-model="code[index]" @input="handleInput" @keypress="isNumber"
-                            @keydown.delete="handleDelete" @paste="onPaste" />
-                    </div>
-                    <p v-if="!isResendLink" class="resend-plain">Resend the verification code in {{ countDown }}s.</p>
-                    <a v-if="isResendLink" class="resend" @click="resendOTP">Resend the verification code.</a>
-                    <Button class="cta-btn" style="margin-bottom: 0px;" type="primary" :loading="isLoading" :disabled="isDisabled" @click="verifyOTP">
-                        Verify
-                    </Button>
-                    <p v-if="isRateExceeded" style="color: #FF5C5C; text-align: center;">Too many attempts. Please wait for five minutes.</p>
-                    <p v-if="otpValidataionFailed" style="color: #FF5C5C; text-align: center;">Invalid email or otp. Please try again.</p>
-                </div>
+            <p class="or"><span>or</span></p>
+            <div class="email-login">
+                <input type="text" v-model="email" placeholder="Enter Email" name="uname" class="email-input" required>
             </div>
-        </transition>
+            <Button class="cta-btn" type="primary" :loading="isLoadingLogin" :disabled="loginDisabled" @click="emailLogin">
+                Continue
+            </Button>
+        </div>
       </form>
     </div>
   </div>
@@ -56,27 +31,15 @@
 import { ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { Button } from 'ant-design-vue';
-import { twitterSocialLogin, signOutFirebase, genOTP, validateOTP, googleSocialLogin } from '@/services/auth'
+import { googleSocialLogin, emailAnonymousLogin } from '@/services/auth'
 import { useAuthStore } from '@/stores/auth'
 import background from "@/assets/background.png";
-import secure from "@/assets/secure.svg";
-import { createNotification } from '@/web3/utils';
 
 const store = useAuthStore();
 const router = useRouter();
-let code: string[] = Array(6);
-let dataFromPaste: string[] | undefined;
-const keysAllowed: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 const show = ref<boolean>(true);
 const email = ref<string>("");
-const isResendLink = ref<boolean>(false);
-const isRateExceeded = ref<boolean>(false);
-const isDisabled = ref<boolean>(true);
-const otpValidataionFailed = ref<boolean>(false);
-const countDown = ref<number>(60);
 const isLoadingLogin = ref(false);
-const isLoading = ref(false);
-const isTwitterLoading = ref<boolean>(false);
 const isGoogleLoading = ref<boolean>(false);
 const loginDisabled = ref<boolean>(true);
 
@@ -84,131 +47,16 @@ const onSubmit = (e: Event) => {
     e.preventDefault();
 }
 
-const google_login = async () => {
+const googleLogin = async () => {
     isGoogleLoading.value = true;
     await googleSocialLogin();
     router.push(store.returnUrl || "/");
 }
 
-const isNumber = (event: Event) => {
-    (event.currentTarget as HTMLInputElement).value = "";
-    const keyPressed: string = (event as KeyboardEvent).key;
-    if (!keysAllowed.includes(keyPressed)) {
-        event.preventDefault();
-    }
-}
-
-const handleInput = (event: Event) => {
-    const inputType = (event as InputEvent).inputType;
-    let currentActiveElement = event.target as HTMLInputElement;
-    if (currentActiveElement.id.split("_")[1] === "5") {
-        isDisabled.value = false;
-    }
-    if (inputType === "insertText")
-        (currentActiveElement.nextElementSibling as HTMLElement)?.focus();
-    if (inputType === "insertFromPaste" && dataFromPaste) {
-        for (const num of dataFromPaste) {
-            let id: number = parseInt(currentActiveElement.id.split("_")[1]);
-            currentActiveElement.value = num;
-            code[id] = num;
-            if (currentActiveElement.nextElementSibling) {
-                currentActiveElement =
-                    currentActiveElement.nextElementSibling as HTMLInputElement;
-                (currentActiveElement.nextElementSibling as HTMLElement)?.focus();
-            }
-        }
-    }
-}
-
-const handleDelete = (event: Event) => {
-    //keydown event = move to previous element then only delete number
-    let value = (event.target as HTMLInputElement).value;
-    let currentActiveElement = event.target as HTMLInputElement;
-    if (!value)
-        (currentActiveElement.previousElementSibling as HTMLElement)?.focus();
-}
-
-const onPaste = (event: Event) => {
-    dataFromPaste = (event as ClipboardEvent).clipboardData
-        ?.getData("text")
-        .trim()
-        .split("");
-    if (dataFromPaste) {
-        isDisabled.value = false;
-        for (const num of dataFromPaste) {
-            if (!keysAllowed.includes(num)) event.preventDefault();
-        }
-    }
-}
-
-const countDownTimer = () => {
-    let interval = setInterval(() => {
-        if (countDown.value > 0) {
-            countDown.value--;
-        } else {
-            clearInterval(interval);
-            isResendLink.value = true;
-        }
-    }, 1000)
-}
-
-const sendOTP = async () => {
-    if(email.value != "") {
-        isLoadingLogin.value = true;
-        try {
-            const result = await genOTP(email.value, "signin");
-            if (result === 429) {
-                console.error("Too many requests to send otp.");
-                createNotification("Too many requests to send otp.", "error");
-            }
-            else if (result === 200) {
-                show.value = !show.value;
-                email.value = email.value.toLowerCase();
-                countDownTimer();
-            }
-        } catch (err) {
-            console.log(err);
-            createNotification(err as string, "error");
-        } finally {
-            isLoadingLogin.value = false;
-        }
-    } else {
-        createNotification("Please enter email to continue", "error");
-    }
-}
-
-const resendOTP = async() => {
-    countDown.value = 60;
-    isResendLink.value = false;
-    countDownTimer();
-    const result = await genOTP(email.value, "signin");
-    if (result === 429) {
-        console.error("Too many requests to send otp.");
-        createNotification("Too many requests to send otp.", "error");
-    }
-}
-
-const verifyOTP = async () => {
-    isRateExceeded.value = false;
-    otpValidataionFailed.value = false;
-    isLoading.value = true;
-    try {
-        const result = await validateOTP(email.value, code.join(""));
-        if (result?.code === 200) {
-            router.push(store.returnUrl || "/");
-        } else if (result?.code === 429) {
-            isRateExceeded.value = true;
-        } else if (result?.code === 400) {
-            if (!isRateExceeded.value) {
-                otpValidataionFailed.value = true;
-            }
-        }
-    } catch (err) {
-        console.log(err);
-        createNotification(err as string, "error");
-    } finally {
-        isLoading.value = false;
-    }
+const emailLogin = async() => {
+    isLoadingLogin.value = true;
+    await emailAnonymousLogin(email.value);
+    router.push(store.returnUrl || "/");
 }
 
 watch(email, () => {
